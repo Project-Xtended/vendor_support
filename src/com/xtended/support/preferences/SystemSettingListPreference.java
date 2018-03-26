bcbf04ebcdcacbd7f5c674a65f67b1079fc208a2
SpiritCroc <spiritcroc@gmail.com>
2018-03-25 19:50:45 -0700

    BenzoSettings: Add seekbar and list settings preferences
    Taken from AICP

--- src/com/xtended/support/preferences/SystemSettingListPreference.java
+++ src/com/xtended/support/preferences/SystemSettingListPreference.java
@@ -20,8 +20,6 @@ import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 
-import androidx.preference.ListPreference;
-
 public class SystemSettingListPreference extends ListPreference {
 
     public SystemSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
